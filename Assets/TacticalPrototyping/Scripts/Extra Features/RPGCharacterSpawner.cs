7f1b0c050d17c450b0b161332311cd6113881df0
Tanner555 <theman989898@gmail.com>
2019-08-13 20:15:07 -0700

    Created RPGAllyComponentSetupObject scriptable object

--- Assets/TacticalPrototyping/Scripts/Extra Features/RPGCharacterSpawner.cs
+++ Assets/TacticalPrototyping/Scripts/Extra Features/RPGCharacterSpawner.cs
@@ -9,9 +9,54 @@ using BaseFramework;
 namespace RPGPrototype { 
     public class RPGCharacterSpawner : CharacterSpawner
     {
+        #region CharacterSetupFields
+        //Character Setup Fields
+        [Header("Character Setup Fields")]
+        [Header("Ally Instance Setup Fields")]
+        [SerializeField]
+        protected RTSAllyComponentSpecificFields AllySpecificComponentsToSetUp;
+
+        [Header("All Allies Setup Fields")]
+        [SerializeField]
+        protected RPGAllyComponentSetupObject AllAllyComponentFieldsObject;
+
+        protected RTSAllyComponentsAllCharacterFields AllAllyComponentFields
+        {
+            get { return AllAllyComponentFieldsObject.AllyComponentSetupFields; }
+        }
+        #endregion
+
         #region Properties
         RPGGameMode gamemode => RPGGameMode.thisInstance;
         RPGGameMaster gamemaster => RPGGameMaster.thisInstance;
+        #endregion
+
+        #region CharacterBuilder_BuildCharacter
+        protected override IEnumerator CharacterBuilder_BuildCharacter()
+        {
+            yield return new WaitForSeconds(0.05f);
+        }
+        #endregion
+
+        #region CharacterSetup_SetupCharacter
+        protected override IEnumerator CharacterSetup_SetupCharacter()
+        {
+            yield return new WaitForSeconds(0f);
+        }
+        #endregion
+
+        #region ItemBuilder_BuildItem
+        protected override IEnumerator ItemBuilder_BuildItem()
+        {
+            yield return new WaitForSeconds(0f);
+        }
+        #endregion
+
+        #region CharacterBuilder_UpdateCharacter
+        protected override IEnumerator CharacterBuilder_UpdateCharacter()
+        {
+            yield return new WaitForSeconds(0f);
+        }
         #endregion
 
         #region CharacterSetup_UpdateCharacterSetup
