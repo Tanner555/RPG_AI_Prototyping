c7549626a0074d401c5bfd927726459f953d0a1a
Tanner555 <theman989898@gmail.com>
2019-12-30 06:30:22 -0800

    Added MoveDirFromNavDestination Task

--- Assets/TacticalPrototyping/Scripts/BehaviorScripts/Actions/ResetCharacterNavMovement.cs
+++ Assets/TacticalPrototyping/Scripts/BehaviorScripts/Actions/ResetCharacterNavMovement.cs
@@ -2,73 +2,89 @@ using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 using RTSCoreFramework;
+using UnityEngine.AI;
 
 namespace RPGPrototype
 {
 	[TaskCategory("RPGPrototype/AllyMember")]
-    [TaskDescription("Resets The Provided Character Navigation Movement BlackBoard Variables.")]
+    [TaskDescription("Resets The Provided Character Navigation Movement BlackBoard Variables and NavMeshAgent Dest.")]
 	public class ResetCharacterNavMovement : Action
 	{
-		#region Properties
-		public SharedVector3 MyNavDestination;
+        #region Shared
+        public SharedVector3 MyNavDestination;
 		public SharedBool bHasSetDestination;
 		public SharedBool bHasSetCommandMove;
+        #endregion
 
-		//AllyMember allyMember
-		//{
-		//	get
-		//	{
-		//		if(_allymember == null)
-		//		{
-		//			_allymember = GetComponent<AllyMember>();
-		//		}
-		//		return _allymember;
-		//	}
-		//}
-		//AllyMember _allymember = null;
+        #region Properties
+        NavMeshAgent navMeshAgent
+		{
+			get
+			{
+				if(_navMeshAgent == null)
+				{
+					_navMeshAgent = GetComponent<NavMeshAgent>();
+				}
+				return _navMeshAgent;
+			}
+		}
+		NavMeshAgent _navMeshAgent = null;
 
-		//AIControllerRPG aiController
-		//{
-		//	get
-		//	{
-		//		if(_aiController == null)
-		//		{
-		//			_aiController = (AIControllerRPG)allyMember.aiController;
-		//		}
-		//		return _aiController;
-		//	}
-		//}
-		//AIControllerRPG _aiController = null;
+        AllyMember allyMember
+        {
+            get
+            {
+                if (_allymember == null)
+                {
+                    _allymember = GetComponent<AllyMember>();
+                }
+                return _allymember;
+            }
+        }
+        AllyMember _allymember = null;
 
-		//AllyEventHandler myEventHandler
-		//{
-		//	get
-		//	{
-		//		if(_myEventhandler == null)
-		//		{
-		//			_myEventhandler = GetComponent<AllyEventHandler>();
-		//		}
-		//		return _myEventhandler;
-		//	}
-		//}
-		//AllyEventHandler _myEventhandler = null;
+        AIControllerRPG aiController
+        {
+            get
+            {
+                if (_aiController == null)
+                {
+                    _aiController = (AIControllerRPG)allyMember.aiController;
+                }
+                return _aiController;
+            }
+        }
+        AIControllerRPG _aiController = null;
 
-		//BehaviorTree AllyBehaviorTree
-  //      {
-  //          get
-  //          {
-  //              if(_AllyBehaviorTree == null)
-  //              {
-  //                  _AllyBehaviorTree = GetComponent<BehaviorTree>();
-  //              }
-  //              return _AllyBehaviorTree;
-  //          }            
-  //      }
-  //      BehaviorTree _AllyBehaviorTree = null;
-		#endregion
+        AllyEventHandler myEventHandler
+        {
+            get
+            {
+                if (_myEventhandler == null)
+                {
+                    _myEventhandler = GetComponent<AllyEventHandler>();
+                }
+                return _myEventhandler;
+            }
+        }
+        AllyEventHandler _myEventhandler = null;
 
-		#region Overrides
-		public override void OnStart()
+        BehaviorTree AllyBehaviorTree
+        {
+            get
+            {
+                if (_AllyBehaviorTree == null)
+                {
+                    _AllyBehaviorTree = GetComponent<BehaviorTree>();
+                }
+                return _AllyBehaviorTree;
+            }
+        }
+        BehaviorTree _AllyBehaviorTree = null;
+        #endregion
+
+        #region Overrides
+        public override void OnStart()
 		{
 		
 		}
@@ -78,6 +94,8 @@ namespace RPGPrototype
 			MyNavDestination.Value = Vector3.zero;
 			bHasSetDestination.Value = false;
 			bHasSetCommandMove.Value = false;
+            navMeshAgent.SetDestination(transform.position);
+            navMeshAgent.velocity = Vector3.zero;
 			//AllyBehaviorTree.SetVariableValue(aiController.BBName_MyNavDestination, Vector3.zero);
 			//AllyBehaviorTree.SetVariableValue(aiController.BBName_bHasSetDestination, false);
 			//AllyBehaviorTree.SetVariableValue(aiController.BBName_bHasSetCommandMove, false);
@@ -89,6 +107,8 @@ namespace RPGPrototype
 			MyNavDestination.Value = Vector3.zero;
 			bHasSetDestination.Value = false;
 			bHasSetCommandMove.Value = false;
+            navMeshAgent.SetDestination(transform.position);
+            navMeshAgent.velocity = Vector3.zero;
 			//AllyBehaviorTree.SetVariableValue(aiController.BBName_MyNavDestination, Vector3.zero);
 			//AllyBehaviorTree.SetVariableValue(aiController.BBName_bHasSetDestination, false);
 			//AllyBehaviorTree.SetVariableValue(aiController.BBName_bHasSetCommandMove, false);
