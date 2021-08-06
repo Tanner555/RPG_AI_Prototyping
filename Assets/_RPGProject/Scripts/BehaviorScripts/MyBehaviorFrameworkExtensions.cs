66866bada95794d537e49100510eb3dc3952c924
Tanner555 <theman989898@gmail.com>
2021-08-06 02:41:27 -0700

    Update Unity To 2021.1.16f1

--- Assets/_RPGProject/Scripts/BehaviorScripts/MyBehaviorFrameworkExtensions.cs
+++ Assets/_RPGProject/Scripts/BehaviorScripts/MyBehaviorFrameworkExtensions.cs
@@ -24,10 +24,15 @@ namespace RPGPrototype.Behaviors
         /// Call this Method To Init Spawner Because Unity Msgs Have Been Decorated
         /// With bIsInitCustomNoUnityMsgs Boolean To Prevent Starting Before
         /// Graph Has Been Assigned.
+        /// Enter Boolean In uNodeSpawner Class
+        /// public bool bIsInitCustomNoUnityMsgs = false;
+        /// Then Enter This Code First In Awake, OnEnable, and Start Methods
+        /// if (bIsInitCustomNoUnityMsgs == false) return;
+        /// Set These Methods To Public
         /// </summary>
         /// <param name="_spawner"></param>
         public static void CustomSpawnerInitNoUnityMsgs(this MaxyGames.uNode.uNodeSpawner _spawner)
-        {            
+        {
             //Allows Unity Msgs Code To Run Again
             _spawner.bIsInitCustomNoUnityMsgs = true;
             _spawner.Awake();
